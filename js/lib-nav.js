35e3c71276c6fae771dcbc76eea37dc3ee10b9c8
Angga <erlanggasatria6677@gmail.com>
2025-06-28 14:34:56 +0700

    Update lib-nav.js

--- js/lib-nav.js
+++ js/lib-nav.js
@@ -1,1 +1,17 @@
+const initNavbar = () => {
+  const navbarExpand = document.querySelector('[data-expand="navbar"]');
+  const btnExpand = document.querySelector('[data-expand="btn-expand"]');
+  const btnExpandClose = document.querySelector('[data-expand="btn-close"]');
 
+  btnExpand.addEventListener("click", () => {
+    navbarExpand.classList.remove("translate-x-full");
+    navbarExpand.classList.add("translate-x-0");
+  });
+  
+  btnExpandClose.addEventListener("click", () => {
+    navbarExpand.classList.remove("translate-x-0");
+    navbarExpand.classList.add("translate-x-full");
+  });
+};
+
+export { initNavbar };
