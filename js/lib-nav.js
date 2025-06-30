93c8ef791b57d40aa1f2a0ec2d2a2375d2d8a172
Angga <erlanggasatria6677@gmail.com>
2025-06-30 11:55:10 +0700

    Update lib-nav.js

--- js/lib-nav.js
+++ js/lib-nav.js
@@ -1,16 +1,30 @@
+const navbarExpand = document.querySelector('[data-expand="navbar"]');
+const overlay = document.querySelector('[data-expand="overlay"]')
+const btnExpand = document.querySelector('[data-expand="btn-expand"]');
+const btnExpandClose = document.querySelector('[data-expand="btn-close"]');
+
+const closeSidebar = () => {
+  navbarExpand.classList.remove("translate-x-0");
+  navbarExpand.classList.add("translate-x-full");
+  overlay.classList.remove("dsply-block");
+};
+
 const initNavbar = () => {
-  const navbarExpand = document.querySelector('[data-expand="navbar"]');
-  const btnExpand = document.querySelector('[data-expand="btn-expand"]');
-  const btnExpandClose = document.querySelector('[data-expand="btn-close"]');
+  navbarExpand.classList.add("z-idx-30");
+  overlay.classList.add("overlay")
+  
+  overlay.addEventListener("click", () => {
+    closeSidebar();
+  });
 
   btnExpand.addEventListener("click", () => {
     navbarExpand.classList.remove("translate-x-full");
     navbarExpand.classList.add("translate-x-0");
+    overlay.classList.add("dsply-block");
   });
   
   btnExpandClose.addEventListener("click", () => {
-    navbarExpand.classList.remove("translate-x-0");
-    navbarExpand.classList.add("translate-x-full");
+    closeSidebar();
   });
 };
 
